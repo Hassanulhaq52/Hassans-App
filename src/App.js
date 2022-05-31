e38345ee5d227dd51349eb3aa32b531b47bad379
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-05-31 23:24:20 +0500

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+let name = 'Hassan'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+
+      <div className="container"></div>
+
+<h1>Hello {name}</h1>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem esse,
+        beatae iure nam, impedit pariatur ut ipsam tempore corporis voluptatibus
+        voluptatum, enim velit eveniet ipsa facilis. Odit architecto quaerat
+        reiciendis.
+      </p>
+    </>
   );
 }
 
