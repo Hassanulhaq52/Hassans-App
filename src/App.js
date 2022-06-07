96fb2778ab1be8ca8609b463216d3eaaa0be78c7
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-06-07 17:38:48 +0500

    Exercise 2_ Solutions

--- src/App.js
+++ src/App.js
@@ -32,14 +32,26 @@ setTimeout(() => {
 
 }
 
-const toggleMode = () => {
+const removeBodyClasses = () => {
+
+document.body.classList.remove('bg-light');
+document.body.classList.remove('bg-dark');
+document.body.classList.remove('bg-warning');
+document.body.classList.remove('bg-danger');
+document.body.classList.remove('bg-success');
+
+}
+
+const toggleMode = (cls) => {
+
+  removeBodyClasses();
+  document.body.classList.add(('bg-' +cls))
 
 if (mode === 'light') {
 
   setMode ('dark');
   document.body.style.backgroundColor = '#042743';
   showAlert('Dark Mode has been Enabled', 'Success');
-  // document.title = 'Hassans App - dark Mode';
   
 } 
 
@@ -48,7 +60,6 @@ else {
  setMode ('light');
  document.body.style.backgroundColor = 'white';
  showAlert('Light Mode has been Enabled', 'Success');
-//  document.title = 'Hassans App - Light Mode';
 
 }
 
